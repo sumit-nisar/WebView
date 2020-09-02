c6724dc7ceaf5cf00fa7acfcdfd7e4e3700932dc
Sumit Nisar <sumitnisar786@gmail.com>
2020-09-02 17:09:23 +0530

    WebView

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView= findViewById(R.id.webview);
+        //to load this within app
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.co.in/");
+
+        // to change Web settings
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+}
